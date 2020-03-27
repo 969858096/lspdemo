4194a13749fbed7a8c831194e0a162df8501920e
lsp <969858096@qq.com>
2020-03-27 18:34:56 +0800

    测试创建项目提交git

---
+++ src/main/java/com/lsp/lspdemo/LspdemoApplication.java
@@ -0,0 +1,13 @@
+package com.lsp.lspdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LspdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LspdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lsp/lspdemo/LspdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsp.lspdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LspdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
